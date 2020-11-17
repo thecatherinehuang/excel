849108db6b88a3eef48a052cd6c84e73f3bdb73c
Catherine Huang <catherinehuang2018@gmail.com>
2020-11-17 11:09:43 -0800

    all functionality included

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// I defined formula behavior to not change when we change the cells afterwards.
+// I defined formula behavior to not change even after we change the cells it depends on.
 // for example, if C4 = A1 + A2 where initially A1 = 2 and A2 = 3, but later on we modify A1 = 4, C4 remains 5
 // If we wanted to implement the other method, we would need to store each expression as a string and each time we change a cell,
 // in order to display the spreadsheet accurately, we would need to recalculate all expressions that use the cell we're using. 
 // A hashmap would be helpful to keep track of which cells depend on other cells so we don't need to check all 26 * 50 cells each time 
-// we reload, but only the ones we change. This choice also mitigated another problem that we would have possibly had such as 
-// when A1 = SUM(A2, A3) where A2 = SUM(A1, A3) and A3 = 2. 
+// we reload, but only the expressions that depend on the cell we changed. This choice also mitigated another problem that we would 
+// have possibly had such as when A1 = SUM(A2, A3) where A2 = SUM(A1, A3) and A3 = 2. 
 
 // There are definitely some adversarial user inputs that I didn't catch because I was running out of time but I am aware exist.
-// An inexhaustive list of inputs this program will not behave the way expected '=SUM(A1,,,A6)'. potentially, 
+// An inexhaustive list of inputs this program will not behave the way expected '=SUM(A1,,,A6)', '=SUM(A1).
 
 // If math function finds a string, I chose to alert the user, but not crash. 
 
@@ -18,18 +18,17 @@
 
 // Make sure there is a way to delete numbers. If we wanted to delete A5, we can type in A5 as the expression.
 
-// Finally, I deeply apologize for not writing test cases, but rest assured that I tested as I went to make sure my functions were
-// doing what I intended them to do. Things got super crazy busy last week in school so I couldn't start working on this developer 
-// challenge until pretty late. This is my first time touching Javascript (so learning that took some time) and I couldn't figure out 
-// how to test the code in time. I'd love feedback on things I could have done better in writing this code. I'm sure there are some
-// functions that would've made my life easier that I didn't know existed. 
+// Finally, about the test cases. I tested as I went to make sure my functions were doing what I intended them to do, but I wasn't
+// able to figure out how node.js worked. This is my first time touching Javascript, so any feedback is much appreciated. 
 
-// This was super fun. I loved the semi-open-endedness of this project.
+// This was super fun. There's nothing quite like banging your head against code and seeing the page load the way you intended it to. 
+// I never thought I'd be so excited to see a for loop of numbers show up on the screen. 
 
 // Things left to do: 
 // - parse the inputs for SUM/AVG
 // - take into account the strings
 // - check when things are NULL
+// - clean up redundant code
 
 // contains strings, floats, integers, formulas (but we can represent everything as a string for consistency)
 let tableArr = Array.from(Array(26), () => new Array(50));
@@ -48,18 +47,22 @@ function parseIndex(cellIndex){
     // function takes in a string and returns the array indices or alerts
     let row = cellIndex[0]; // should be A-Z
     if (!row){
-        alert('Blank Cell index')
+        alert('Blank Cell index');
+        return false;
     }
     else if (row.charCodeAt(0) < 65 || row.charCodeAt(0) > 90){
-        alert('Please enter a letter between A and Z')
+        alert('Please enter a letter between A and Z');
+        return false
     } 
     
     let column = parseInt(cellIndex.slice(1));
     if (!column){ // case where someone enters AA50, applying parseInt on a not int outputs a NaN
         alert('Please enter a Cell Index in the format {A-Z} {1-50}');
+        return false;
     }
     else if (column < 1 || column > 50 ){
         alert('Please enter a number between 1 and 50');
+        return false;
     }
 
     return [row.charCodeAt(0) - 'A'.charCodeAt(0), column-1]
@@ -67,14 +70,84 @@ function parseIndex(cellIndex){
 
 function parseIndices(expression){
     // either a range, or adding a series of values (split by commas)
-}
+    // enter in 10:20
+    if (expression.includes(':')){
+        let parts = expression.split(':');
+        if (parts.length !== 2){
+            alert('Invalid input. For a range of values, the inner expression should be \'cell:cell\'');
+            return false;
+        }
+        else {
+            if (!parseIndex(parts[0])){
+                return false;
+            } else{
+                [row0, col0] = parseIndex(parts[0]);
+            }
 
+            if (!parseIndex(parts[1])){
+                return false;
+            } else {
+                [row1, col1] = parseIndex(parts[1]);
+            }
+
+            if (row0 === row1 & col0 < col1){
+                let arr = [];
+                for (let col of range(col0, col1)){
+                    let elt = queryTableArr(row0, col);
+                    if (!Number.isNaN(Number(elt))){ // is a number
+                        arr.push(elt);
+                    }
+                    else {
+                        alert('Table array contains a string or is empty.');
+                        return false;
+                    }
+                }
+                return arr;
+            } else if (col0 === col1 & row0 < row1){
+                let arr = [];
+                for (let row of range(row0, row1)){
+                    let elt = queryTableArr(row, col0);
+                    if (!Number.isNaN(Number(elt))){ // is a number
+                        arr.push(elt);
+                    }
+                    else {
+                        alert('Table array contains a string or is empty.');
+                        return false;
+                    }
+                }
+                return arr;
+            } else {
+                alert('Invalid range. Make sure matches in either row and column and indices go from small to large.');
+                return false;
+            }
+        }
+    } else if (expression.includes(',')) { 
+        let parts = expression.split(',');
+        let arr = []
+        for (let part of parts){
+            if (Number.isNaN(Number(part))){ // if it isn't a number 
+                if (parseIndex(part)){ // returns false if input has bugs, will alert
+                    [i,j] = parseIndex(part);
+                    if (!Number.isNaN(Number(queryTableArr(i, j)))){ // check if the queried value is a number
+                        arr.push(Number(queryTableArr(i,j)));
+                    } else {
+                        alert('Table array contains a string or is empty') 
+                        return false;
+                    }
+                } 
+            } else {
+                arr.push(Number(part));
+            }
+        }
+        return arr;
+    }
+}
 
 function basicMathTwoCells(expression, operation){
     let [one, two] = expression.split(operation);
     let [row1, col1] = parseIndex(one);
     let [row2, col2] = parseIndex(two);
-    return [Number(queryTableArr(row1, col1)), Number(queryTableArr(row2, col2))]
+    return [queryTableArr(row1, col1), queryTableArr(row2, col2)]
 }
 
 function queryTableArr(row, col){
@@ -89,7 +162,7 @@ function setTableArr(row, col, val){
 }
 
 function isNumber(expression){
-    if (Number(expression)){
+    if (expression){
         return true;
     } else {
         return false;
@@ -107,74 +180,86 @@ function editContents(form){
     if (!expression){
         alert('Enter an expression');
     }
+    console.log(expression[0] === '=');
     if (expression[0] === '='){
         // figure out which type of expression, call that function
         // any expression that requires evaluating 
         // switch statement 
         let newExpression = expression.slice(1);
-        // switch (newExpression.slice(0, 3)){
-        //     case 'SUM':
-
-        //     case 'AVG':
-
-        // }
-    }
-    else{
         if (expression.includes('+')) {
-            let [a, b]= basicMathTwoCells(expression, '+');
+            let [a, b]= basicMathTwoCells(newExpression, '+');
             if (isNumber(a) & isNumber(b)) {
                 let num = a + b;
-                console.log('entered');
                 setTableArr(i, j, num.toString()); 
             }
             else {
-                alert('Either a specified index is unfilled or a string.')
+                alert('Either a specified index is unfilled or a string.');
+                return false;
             }
         }
         else if (expression.includes('-')){
-            let [a, b]= basicMathTwoCells(expression, '-');
+            let [a, b]= basicMathTwoCells(newExpression, '-');
             if (isNumber(a) & isNumber(b)) {
                 let num = a-b;
                 setTableArr(i, j, num.toString());
             }
             else {
-                alert('Either a specified index is unfilled or a string.')
+                alert('Either a specified index is unfilled or a string.');
+                return false;
             }
         } 
         else if (expression.includes('*')){
-            let [a, b]= basicMathTwoCells(expression, '*');
+            let [a, b]= basicMathTwoCells(newExpression, '*');
             if (isNumber(a) & isNumber(b)) {
                 let num =  a * b;
                 setTableArr(i, j, num.toString());
             }
             else {
-                alert('Either a specified index is unfilled or a string.')
+                alert('Either a specified index is unfilled or a string.');
+                return false;
             }
         } 
         else if (expression.includes('/')){
-            let [a, b]= basicMathTwoCells(expression, '/');
+            let [a, b]= basicMathTwoCells(newExpression, '/');
             if (isNumber(a) & isNumber(b)) {
-                let num = a / b 
-                setTableArr(i, j, num.toString());; 
+                if (b === 0){
+                    alert('Can\'t divide by 0');
+                    return false;
+                }
+                let num = a / b;
+                setTableArr(i, j, num.toString());
             }
             else {
-                alert('Either a specified index is unfilled or a string.')
+                alert('Either a specified index is unfilled or a string.');
             }
         }
-        else{ // when 
-            setTableArr(i, j, expression);
+
+        switch (newExpression.slice(0, 3)){
+            case 'SUM':
+                console.log(newExpression.slice(4, newExpression.length-1));
+                if (parseIndices(newExpression.slice(4, newExpression.length-1))){
+                    let toSumArr = parseIndices(newExpression.slice(4, newExpression.length-1));
+                    setTableArr(i, j, toSumArr.reduce(function(a, b){return a+b;}));
+                    console.log(toSumArr);
+                    console.log(toSumArr.reduce(function(a, b){return a+b;}));
+                }
+                break;
+            case 'AVG':
+                if (parseIndices(newExpression.slice(4, expression.length-1))){
+                    let toSumArr = parseIndices(newExpression.slice(4, expression.length-1));
+                    setTableArr(i, j, toSumArr.reduce(function(a, b){return a+b;}) / toSumArr.length);
+                }
+                break;
         }
-        console.log(tableArr[1])
     }
-    
-    // do DOM traversal to find what to alter
+    else{ // just a number or a string
+        setTableArr(i, j, expression);
+    }
 }
 
 function initializeTable(){
     // tableArr[0] = ["", "A", "B"]
     let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // letters.charCodeAt(0) = 65, A-Z is 65-90
-
     for (let i of range(0, letters.length)){
         for (let j of range(1, 51)){
             tableArr[i][j-1] = letters.charAt(i) + j.toString();
@@ -185,17 +270,24 @@ function initializeTable(){
 
 function createTable(){
     let table = document.createElement('table');
-    tableArr = JSON.parse(localStorage.getItem("spreadsheet"))
+    tableArr = JSON.parse(localStorage.getItem("spreadsheet"));
     // create the table object
+    // is it possible to pick out specific DOM elements and change it instead of reloading the entire table each time?
+    // definitely possible. I'm unsure about how page refreshes would work though. 
     for (let row of tableArr){
         let rowNode = table.insertRow() 
         for (let elt of row){
             let newCell = rowNode.insertCell();
             let text = document.createTextNode(elt);
-            newCell.appendChild(text)
+            newCell.appendChild(text);
         }
     }
     document.body.appendChild(table);
+}
+
+function deleteTable(){
+    let table = document.getElementsByTagName('table')[0];
+    table.parentNode.removeChild(table);
 }
 
 document.body.onload = createTable;
